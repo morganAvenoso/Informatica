083a5e15d160637f91218f307c1aa96c80d49467
morganAvenoso <113618775+morganAvenoso@users.noreply.github.com>
2022-12-22 22:03:15 +0100

    Create index.php

---
+++ ES_04_Session/a/index.php
@@ -0,0 +1,23 @@
+<?php session_start();?>
+<html>
+<head>
+    <h1>WebApp</h1>
+</head>
+<body>
+<?php
+    if($_SESSION['id']=="pluto"){
+        echo "Hai già effettuato l'accesso ".$_SESSION['id']."<br>";
+        ?>
+        <a href="riservata.php">Home profilo</a><br>
+        <a href="logout.php">Logout</a><br>
+        <?php   
+    }else{
+        echo "Non hai accesso alla pagina<br>"
+        ?>
+        <a href="login.php">Login</a><br>
+        <a href="riservata.php">Home profilo</a><br>
+        <?php
+    }
+?>
+</body>
+</html>
